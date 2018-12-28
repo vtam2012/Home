fc52481fbc1705671178eb84e61d877e79e3a3a4
Vincent Tam <vtam2012@gmail.com>
2018-12-27 20:07:40 -0800

    Updates

---
+++ precache-manifest.19ec7e8acdd8525ca95384b1e0a97fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "1880f947fb744eaa3990",
+    "url": "/home/static/js/main.1880f947.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/home/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1880f947fb744eaa3990",
+    "url": "/home/static/css/main.0c217c7b.chunk.css"
+  },
+  {
+    "revision": "1b882e37cc8eea0c106cbcd763165894",
+    "url": "/home/index.html"
+  }
+];
